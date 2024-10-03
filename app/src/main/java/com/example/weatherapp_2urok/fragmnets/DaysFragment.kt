6a8457f9c7e5c6899138e11cb2fb2ddd4d193b4d
Agressste <katya_klimentova@bk.ru>
2024-10-04 01:16:18 +0900

    urok17

--- app/src/main/java/com/example/weatherapp_2urok/fragmnets/DaysFragment.kt
+++ app/src/main/java/com/example/weatherapp_2urok/fragmnets/DaysFragment.kt
@@ -11,9 +11,10 @@ import androidx.recyclerview.widget.ListAdapter
 import com.example.weatherapp_2urok.MainViewModule
 import com.example.weatherapp_2urok.R
 import com.example.weatherapp_2urok.adapter.WeatherAdapter
+import com.example.weatherapp_2urok.adapter.WeatherModel
 import com.example.weatherapp_2urok.databinding.FragmentDaysBinding
 
-class DaysFragment : Fragment() {
+class DaysFragment : Fragment(), WeatherAdapter.Listener {
     private lateinit var adapter: WeatherAdapter
     private lateinit var binding: FragmentDaysBinding
     private val model: MainViewModule by activityViewModels()
@@ -35,7 +36,7 @@ class DaysFragment : Fragment() {
     }
 
     private fun init() = with(binding){
-        adapter = WeatherAdapter()
+        adapter = WeatherAdapter(this@DaysFragment)
         rcView.layoutManager = LinearLayoutManager(activity)
         rcView.adapter= adapter
     }
@@ -43,5 +44,9 @@ class DaysFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = DaysFragment()
+    }
+
+    override fun onClick(item: WeatherModel) {
+        model.liveDataCurrent.value = item
     }
 }
--- app/src/main/java/com/example/weatherapp_2urok/fragmnets/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp_2urok/fragmnets/MainFragment.kt
@@ -66,9 +66,9 @@ class MainFragment : Fragment() {
             val maxMinTemp = "${it.maxTemp}°C/${it.minTemp}°C"
             tvData.text = it.time
             tvCity.text = it.city
-            tvCurrentTemp.text = it.currentTemp
+            tvCurrentTemp.text = it.currentTemp.ifEmpty { maxMinTemp }
             tvCondition.text = it.condition
-            tvMaxMin.text = maxMinTemp
+            tvMaxMin.text = if (it.currentTemp.isEmpty()) "" else maxMinTemp
             Picasso.get().load("https:" + it.imageURL).into(imWeather)
         }
     }
@@ -128,8 +128,8 @@ class MainFragment : Fragment() {
                 day.getString("date"),
                 day.getJSONObject("day").getJSONObject("condition").getString("text"),
                 "",
-                day.getJSONObject("day").getString("maxtemp_c"),
-                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getString("maxtemp_c").toFloat().toInt().toString(),
+                day.getJSONObject("day").getString("mintemp_c").toFloat().toInt().toString(),
                 day.getJSONObject("day").getJSONObject("condition")
                     .getString("icon"),
                 day.getJSONArray("hour").toString()
